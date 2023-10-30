4572caa464d59ee6a431c4c43b1dca1f4de7b7fa
haseHH <christian@hase.hamburg>
2023-10-30 18:26:03 +0100

    Add description of the tracking function

--- fetcher/src/main.py
+++ fetcher/src/main.py
@@ -22,8 +22,16 @@ app = FastAPI(
     ],
 )
 
-@app.get("/dpd", tags=["carriers"])
+@app.get("/dpd", tags=["carriers"], summary="DPD Germany")
 def dpd(parcelno: str, zip: str | None = None, locale: str = "en_US"):
+    """
+    Fetch the tracking info of a package carried by DPD Germany.
+
+    - **parcelno**: The number of your parcel.
+    - **zip**: The ZIP code of the recipient. Won't grant any more detail in the tracking status, but setting it will include it in the `weblink`, saving you a step when opening the official tracking page.
+    - **locale**: Specifies the language of the status labels and descriptions, default is `en_US`, other known options include: `de_DE`, `fr_FR`
+    """
+
     r = requests.get(f"https://tracking.dpd.de/rest/plc/{locale}/{parcelno}")
 
     if (zip == None):
